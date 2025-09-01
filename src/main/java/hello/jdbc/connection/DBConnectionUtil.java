0201dac72fc7a25b8c47f79fe3ea8887aea4c3ba
jngbn <[user email]>
2025-09-01 17:56:09 +0900

    save

--- src/main/java/hello/jdbc/connection/DBConnectionUtil.java
+++ src/main/java/hello/jdbc/connection/DBConnectionUtil.java
@@ -10,6 +10,7 @@ import static hello.jdbc.connection.ConnectionConst.*;
 @Slf4j
 public class DBConnectionUtil {
     public static Connection getConnection(){
+
         try{
             Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             log.info("get connection={}, class={}",connection,connection.getClass());
